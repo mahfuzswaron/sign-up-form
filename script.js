415e8f33695ba3714b870fa0c9c5cbcd7f5f5383
mahfuzswaron <mahfuzswaron@gmail.com>
2024-03-04 15:50:15 +0600

    sign up form completed

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const form = document.getElementById("form");
 
 
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fullName = form.name;
-    const email = form.email;
-    const password = form.password;
+    const fullName = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const dateOfBirth = form.date_of_birth.value;
+    const phone = form.phone.value;
+    const gender = form.gender.value;
 
-    if (password.value.length < 8) {
-        alert("password must contain at leas 8 characters");
-        return 
+    const emailPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+
+    if (emailPattern.test(email) === false) {
+        alert("Enter a valid email address");
+        return;
     }
 
+    // const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
 
-    // console.log(`Name: ${fullName.value}, Email: ${email.value}, Password: ${password.value}`);
+    // const passPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\da-zA-Z]).{8,15}$/;
 
-    alert(`Sign up successfull! Name: ${fullName.value}, Email: ${email.value}, Password: ${password.value}`);
+    const passPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\da-zA-Z]).{8,15}$/;
 
+    if (!passPattern.test(password)) {
+        console.log("incorrect", password)
+        alert("Your password should contain at least 8 characters, 1 digit, 1 special character, mix of uppercase and lowercase letters");
+        return;
+    }
+
+    const user = {};
+
+    user.fullName = fullName;
+    user.email = email;
+    user.phone = phone;
+    user.dateOfBirth = dateOfBirth;
+    user.gender = gender;
+    user.password = password;
+
+    console.log("sign up completed!");
+    console.log(user);
 })
 
